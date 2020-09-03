f1239afd19a1e2f3228fe535485ed7ef56059b22
Lauren Stein <lmstein22@gmail.com>
2020-09-03 15:20:41 -0400

    Switched to my data set from the PythonAPI challenge to match plots

--- WebVisualizations/data.py
+++ WebVisualizations/data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the csv file in
-df = pd.read_csv("Resources/cities.csv")
+df = pd.read_csv("Resources/lms_assets/City_Data.csv")
 
 # Save to file
 df.to_html('data_convert.html', index=False)
